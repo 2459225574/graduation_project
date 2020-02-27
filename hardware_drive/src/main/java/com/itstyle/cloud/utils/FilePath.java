d76c212c00c76fa49442b08e60c647e824864b43
2459225574 <2459225574@qq.com>
2020-02-27 23:07:29 +0800

    hardware drive修改完成，下一步添加用户登陆

---
+++ hardware_drive/src/main/java/com/itstyle/cloud/utils/FilePath.java
@@ -0,0 +1,11 @@
+package com.itstyle.cloud.utils;
+
+/**
+ * @Author chenchao
+ * @Date 23:07 2020/2/27
+ * @Description
+ * @Param
+ * @return
+ **/
+public class FilePath {
+}
---
+++ hardware_drive/src/main/java/com/itstyle/cloud/web/FileListController.java
@@ -0,0 +1,19 @@
+package com.itstyle.cloud.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author chenchao
+ * @Date 23:04 2020/2/27
+ * @Description 用户文件列表
+ * @Param
+ * @return
+ **/
+
+@Controller
+@RequestMapping("/filelist")
+public class FileListController {
+
+
+}
